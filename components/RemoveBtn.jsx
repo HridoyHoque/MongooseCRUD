6cb916003a91e2e9e7f3694e5a89c651d26c5388
HridoyHoque <hhridoy155@gmail.com>
2023-08-26 22:46:49 +0600

    implemented delete method

--- components/RemoveBtn.jsx
+++ components/RemoveBtn.jsx
@@ -1,13 +1,32 @@
+'use client'
+
 import React from 'react';
 import {HiOutlineTrash} from 'react-icons/hi'
-const RemoveBtn = () => {
+
+import { useRouter } from 'next/navigation';
+
+export default function RemoveBtn ({id}) {
+const router = useRouter()
+const removeTopic = async () => {
+    const confirmed = confirm('Are you sure you want to remove?');
+
+    if(confirmed){
+      const res =   await fetch(`http://localhost:3000/api/topics?id=${id}`, {
+            method: 'DELETE',
+        });
+
+        if(res.ok){
+            router.refresh();
+        }
+    }
+}
     return (
    <>
-   <button className='text-red-400'>
+   <button onClick={removeTopic} className='text-red-400'>
     <HiOutlineTrash size={24}/>
     </button>
    </>
     );
 };
 
-export default RemoveBtn;
+// export default RemoveBtn;
