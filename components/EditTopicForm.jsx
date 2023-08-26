0b37fd7260589874d583470c24ce4b54213d7555
HridoyHoque <hhridoy155@gmail.com>
2023-08-26 23:16:45 +0600

    implemented PUT method and completed CRUD Operations

--- components/EditTopicForm.jsx
+++ components/EditTopicForm.jsx
@@ -1,14 +1,50 @@
-import React from 'react';
+'use client'
 
-const EditTopicForm = () => {
+import { useState } from "react";
+import { useRouter } from "next/navigation";
+
+export default function EditTopicForm ({id, title, description}) {
+
+    const [newTitle, setNewTitle] = useState(title)
+    const [newDescription, setNewDescription] = useState(description)
+
+    const router = useRouter();
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+    
+        try {
+          const res = await fetch(`http://localhost:3000/api/topics/${id}`, {
+            method: "PUT",
+            headers: {
+              "Content-type": "application/json",
+            },
+            body: JSON.stringify({ newTitle, newDescription }),
+          });
+    
+          if (!res.ok) {
+            throw new Error("Failed to update topic");
+          }
+    
+          router.refresh();
+          router.push("/");
+        } catch (error) {
+          console.log(error);
+        }
+      };
     return (
-        <form className='flex flex-col gap-3'>
+        <form onSubmit={handleSubmit}  className='flex flex-col gap-3'>
 
-        <input type="text" 
+        <input 
+        onChange={e => setNewTitle(e.target.value)}
+        value={newTitle}
+        type="text" 
         className='border border-slate-500 px-8 py-2'
         placeholder='Topic Title'
         />
-        <input type="text" 
+        <input 
+         onChange={e => setNewDescription(e.target.value)}
+         value={newDescription}
+        type="text" 
         className='border border-slate-500 px-8 py-2'
         placeholder='Topic Description'
         />
@@ -17,4 +53,4 @@ const EditTopicForm = () => {
     );
 };
 
-export default EditTopicForm;
+// export default EditTopicForm;
